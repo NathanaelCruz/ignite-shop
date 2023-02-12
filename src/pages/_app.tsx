0c571c89e0fefea23b4f65a88391ce4a0fd68367
Nathanael Cruz <nathanael.cruz.fd@gmail.com>
2023-02-12 15:28:51 -0300

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Image from 'next/image'
+import { Roboto } from '@next/font/google'
+
+import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
+
+import logoImg from '@/assets/logo.svg'
+
+const roboto = Roboto({
+  weight: '400',
+  variable: '--roboto',
+  subsets: ['latin']
+})
+
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Container className={roboto.className}>
+    <Header>
+      <Image src={logoImg} alt="" />
+    </Header>
+    <Component {...pageProps} />
+  </Container>
 }
